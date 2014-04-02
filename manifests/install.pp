8cdee3d1916c11bba1d0c34bd7eab4498d589379
Paul Springett <paul@springett.me>
2014-04-02 11:27:00 +0100

    Add ruby devel package to unicorn install

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,11 @@ class unicorn::install {
   package { 'unicorn':
     ensure => installed,
     provider => 'gem',
+    require => Package['ruby1.9.1-dev'],
+  }
+
+  package { 'ruby1.9.1-dev':
+    ensure => present
   }
 
 }
